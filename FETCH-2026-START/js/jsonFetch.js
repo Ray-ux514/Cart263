f1f2e1ef8114aaf0e82a3dffda116540c8f5f310
Ray-ux514 <rayvhaunn@hotmail.com>
2026-03-20 12:16:57 -0400

    Adding code

--- FETCH-2026-START/js/jsonFetch.js
+++ FETCH-2026-START/js/jsonFetch.js
@@ -1,6 +1,17 @@
-window.onload = goApp;
+//window.onload = goApp;
 
-async function goApp(){
-console.log("hello fetch")
+//async function goApp(){
+//console.log("hello fetch")
 
+//}
+
+window.onload = goFetch;
+async function goFetch() {
+  try {
+    let response = await fetch("./files/tests/json");
+    let data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
